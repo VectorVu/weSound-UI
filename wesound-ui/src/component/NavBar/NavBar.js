7e3b9cfde0eead824e04752dd4d0965f06afd539
carosaome <sieunhankiet@gmail.com>
2022-05-26 09:48:08 +0700

    update NavBar UI

---
+++ wesound-ui/src/component/NavBar/NavBar.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {Button, Container,Navbar as bNavBar,} from'react-bootstrap'
+import { Nav, NavDropdown, Form, FormControl} from 'react-bootstrap'
+import './NavBar.css'
+export default function NavBar(){
+
+
+    return(
+        <div>
+        <bNavBar className='navbar navbar-expand-lg navbar-light bg-light Container-NavBar' bg="light" expand="lg">
+                <Container  className='NavBar'>
+                <bNavBar.Brand className='NavBar-brand' href="#">
+                    <img 
+                        src= 'https://a-v2.sndcdn.com/assets/images/peace-cloud-28ad0963.svg'
+                        width="64"
+                        className="d-inline-block align-top NavBar-logo"
+                        alt="React Bootstrap logo"
+                    />
+                </bNavBar.Brand>
+                <bNavBar.Toggle aria-controls="navbarScroll" />
+                <bNavBar.Collapse id="navbarScroll" className='NavBar-Collapse'>
+                    <Nav
+                    className="me-auto my-2 my-lg-0 Navbar-left"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                    >
+                    <Nav.Link href="#action1" className='NavBar-item'>Home</Nav.Link>
+                    <Nav.Link href="#action2" className='NavBar-item'>Stream</Nav.Link>
+                    <Nav.Link href="#action2" className='NavBar-item'>Library</Nav.Link>
+                    
+                    <Form className="d-flex NavBar-form">
+                        <FormControl
+                            type="search"
+                            placeholder="Search for artists, bands, tracks, podcasts"
+                            aria-label="Search"
+                        />
+                        <button className='NabBarForm-button'>
+                            <img src='https://a-v2.sndcdn.com/assets/images/search-dbfe5cbb.svg' />
+                        </button >
+                    </Form>
+                    </Nav>
+                    <NavDropdown title="carosaome" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                        LogOut
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#action2">Upload</Nav.Link>
+                    <Nav.Link href="#action2">
+                        <img 
+                            src='https://a-v2.sndcdn.com/assets/images/activities-66caaa5e.svg'
+                        />
+                    </Nav.Link>
+                    <Nav.Link href="#action2">
+                        <img 
+                            src='https://a-v2.sndcdn.com/assets/images/messages-f517d0eb.svg'
+                        />
+                    </Nav.Link>
+                </bNavBar.Collapse>
+                </Container>
+            </bNavBar>
+        </div>
+  
+        
+        
+
+    )
+}
---
+++ wesound-ui/src/component/Track/Track.js
@@ -0,0 +1,37 @@
+import './Track.css'
+
+export default function Track(){
+
+    return(
+        <div className="Track">
+            <div className="Track-img">
+                <img src="https://avatar-ex-swe.nixcdn.com/playlist/2022/04/29/b/a/8/6/1651215910805_300.jpg"></img>
+            </div>
+            <div className="Track-content">
+                <span className="TrackContent-userName">Carosaome</span>
+                <span>-</span>
+                <span className="TrackContent-trackName">The angel</span>
+                <div className='TrackContent-comment'>
+                    <img src='https://avatar-ex-swe.nixcdn.com/playlist/2022/04/29/b/a/8/6/1651215910805_300.jpg'></img>
+                    <form className='TrackContentComment-form'>
+                        <input placeholder='Write Comment' />
+                    </form>
+                </div>
+                <div className="Track-additional">
+                    <div className='TrackAdditional-likeCount'>
+                        180
+                    </div>
+                    <div className='TrackAdditional-playCount'>
+                        <img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Im5vIj8+DQo8c3ZnIHdpZHRoPSIxNnB4IiBoZWlnaHQ9IjE2cHgiIHZpZXdCb3g9IjAgMCAxNiAxNiIgdmVyc2lvbj0iMS4xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB4bWxuczpza2V0Y2g9Imh0dHA6Ly93d3cuYm9oZW1pYW5jb2RpbmcuY29tL3NrZXRjaC9ucyI+DQogICAgPCEtLSBHZW5lcmF0b3I6IFNrZXRjaCAzLjAuMyAoNzg5MSkgLSBodHRwOi8vd3d3LmJvaGVtaWFuY29kaW5nLmNvbS9za2V0Y2ggLS0+DQogICAgPHRpdGxlPnN0YXRzX3BsYXkgNDwvdGl0bGU+DQogICAgPGRlc2M+Q3JlYXRlZCB3aXRoIFNrZXRjaC48L2Rlc2M+DQogICAgPGRlZnMvPg0KICAgIDxnIGlkPSJQYWdlLTEiIHN0cm9rZT0ibm9uZSIgc3Ryb2tlLXdpZHRoPSIxIiBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiIHNrZXRjaDp0eXBlPSJNU1BhZ2UiPg0KICAgICAgICA8ZyBpZD0ic3RhdHNfcGxheS0iIHNrZXRjaDp0eXBlPSJNU0xheWVyR3JvdXAiIGZpbGw9InJnYigxNTMsIDE1MywgMTUzKSI+DQogICAgICAgICAgICA8cGF0aCBkPSJNNCwxMyBMNCwzIEwxMyw4IEw0LDEzIFoiIGlkPSJzdGF0c19wbGF5LTMiIHNrZXRjaDp0eXBlPSJNU1NoYXBlR3JvdXAiLz4NCiAgICAgICAgPC9nPg0KICAgIDwvZz4NCjwvc3ZnPg0K"></img>
+                        <span className="TrackAdditionnalPlayCount-num">1.2m</span>
+                    </div>
+                    <div className='TrackAdditional-playCount'>
+                        <img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Im5vIj8+DQo8c3ZnIHdpZHRoPSIxNnB4IiBoZWlnaHQ9IjE2cHgiIHZpZXdCb3g9IjAgMCAxNiAxNiIgdmVyc2lvbj0iMS4xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB4bWxuczpza2V0Y2g9Imh0dHA6Ly93d3cuYm9oZW1pYW5jb2RpbmcuY29tL3NrZXRjaC9ucyI+DQogICAgPCEtLSBHZW5lcmF0b3I6IFNrZXRjaCAzLjAuMyAoNzg5MSkgLSBodHRwOi8vd3d3LmJvaGVtaWFuY29kaW5nLmNvbS9za2V0Y2ggLS0+DQogICAgPHRpdGxlPnN0YXRzX3BsYXkgNDwvdGl0bGU+DQogICAgPGRlc2M+Q3JlYXRlZCB3aXRoIFNrZXRjaC48L2Rlc2M+DQogICAgPGRlZnMvPg0KICAgIDxnIGlkPSJQYWdlLTEiIHN0cm9rZT0ibm9uZSIgc3Ryb2tlLXdpZHRoPSIxIiBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiIHNrZXRjaDp0eXBlPSJNU1BhZ2UiPg0KICAgICAgICA8ZyBpZD0ic3RhdHNfcGxheS0iIHNrZXRjaDp0eXBlPSJNU0xheWVyR3JvdXAiIGZpbGw9InJnYigxNTMsIDE1MywgMTUzKSI+DQogICAgICAgICAgICA8cGF0aCBkPSJNNCwxMyBMNCwzIEwxMyw4IEw0LDEzIFoiIGlkPSJzdGF0c19wbGF5LTMiIHNrZXRjaDp0eXBlPSJNU1NoYXBlR3JvdXAiLz4NCiAgICAgICAgPC9nPg0KICAgIDwvZz4NCjwvc3ZnPg0K"></img>
+                        <span className="TrackAdditionnalPlayCount-num">1.2m</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
